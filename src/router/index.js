0fbd6975d2c73b63086c3265d5e717d48ef47217
originalslime4 <original.slime4@gmail.com>
2025-09-06 19:41:58 +0900

    더러운방법

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Base from '../App.vue'
 import Board from '../views/jjal.vue'
 import Home from '../views/home.vue'
+import ReloadPage from '../views/reload.vue'
 // import Gallery from '../views/Gallery.vue'
 
 const routes = [
   { path: '/', name:"App", component: Base },
   { path: '/jjal', name:"jallPage", component: Board },
   { path: '/home', name:"homePage", component: Home },
+  { path: '/reload', component: ReloadPage},
+
 //   { path: '/gallery', component: Gallery }
 ]
 
