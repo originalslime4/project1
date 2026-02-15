292f66b05c8ecd3213214a18740e4fac802f914b
originalslime4 <original.slime4@gmail.com>
2026-02-16 08:52:26 +0900

    버그

--- server.js
+++ server.js
@@ -62,34 +62,34 @@ async function saveFileToDrive(filePath, fileId) {
   console.log("✅ Google Drive 저장 완료:", res.data.id);
 }
 //파일들
-oauth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN
-});
-await oauth2Client.getAccessToken();
-const drive = google.drive({ version: "v3", auth: oauth2Client });
-async function downloadFile(fileId, destPath) {
-  const dest = fs.createWriteStream(destPath);
-  const res = await drive.files.get(
-    { fileId, alt: "media" },
-    { responseType: "stream" }
-  );
-  return new Promise((resolve, reject) => {
-    res.data
-      .on("end", () => {
-        console.log("✅ 다운로드 완료:", destPath);
-        resolve();
-      })
-      .on("error", (err) => {
-        console.error("❌ 다운로드 실패:", err);
-        reject(err);
-      })
-      .pipe(dest);
-  });
-}
-await downloadFile("1VnMzqbM6LYMTeBFzlTUUUT8WwZ_UcKeo", path.join(__dirname, "jjal.js"));
-await downloadFile("1doHeqgBaHQhRIeAFn6KJkarR2EGFyDSB", path.join(__dirname, "user.js"));
-await downloadFile("1oixWdPJTjn8ngSfK5FDzA-ZtmGD9guNK", path.join(__dirname, "follow.js"));
-await downloadFile("1EO2faPd7A_bmPIPk8fiOJQCFMIst5HKB", path.join(__dirname, "like.js"));
+// oauth2Client.setCredentials({
+//   refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+// });
+// await oauth2Client.getAccessToken();
+// const drive = google.drive({ version: "v3", auth: oauth2Client });
+// async function downloadFile(fileId, destPath) {
+//   const dest = fs.createWriteStream(destPath);
+//   const res = await drive.files.get(
+//     { fileId, alt: "media" },
+//     { responseType: "stream" }
+//   );
+//   return new Promise((resolve, reject) => {
+//     res.data
+//       .on("end", () => {
+//         console.log("✅ 다운로드 완료:", destPath);
+//         resolve();
+//       })
+//       .on("error", (err) => {
+//         console.error("❌ 다운로드 실패:", err);
+//         reject(err);
+//       })
+//       .pipe(dest);
+//   });
+// }
+// await downloadFile("1VnMzqbM6LYMTeBFzlTUUUT8WwZ_UcKeo", path.join(__dirname, "jjal.js"));
+// await downloadFile("1doHeqgBaHQhRIeAFn6KJkarR2EGFyDSB", path.join(__dirname, "user.js"));
+// await downloadFile("1oixWdPJTjn8ngSfK5FDzA-ZtmGD9guNK", path.join(__dirname, "follow.js"));
+// await downloadFile("1EO2faPd7A_bmPIPk8fiOJQCFMIst5HKB", path.join(__dirname, "like.js"));
 
 // // const res = await axios.post("http://localhost:10000/analyze-image", {
 //   url: "https://example.com/test.jpg"
